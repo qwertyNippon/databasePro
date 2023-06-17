2dfc5c92a90b70fff5ea13c664cdb411b2edd93c
qwertyNippon <orlandohenry100@gmail.com>
2023-06-17 15:39:58 -0400

    added other stats

--- pokemon.py
+++ pokemon.py
@@ -11,8 +11,11 @@ def func(poke):
     data_dict['name'] = data['name']
     data_dict['abilities'] = data['abilities'][0]['ability']
     data_dict['base_experience'] = data['base_experience']
+    data_dict['attack'] = data['stats'][0]
+    data_dict['attack'] = data['stats'][1]
+    data_dict['defense'] = data['stats'][2]
+    data_dict['special-attack'] = data['stats'][3]
     data_dict['sprites'] = data['sprites']['front_shiny']
-
     print(data_dict)
 
 func('gyarados')
@@ -24,8 +27,11 @@ def func(poke):
     data_dict['name'] = data['name']
     data_dict['abilities'] = data['abilities'][0]['ability']
     data_dict['base_experience'] = data['base_experience']
+    data_dict['attack'] = data['stats'][0]
+    data_dict['attack'] = data['stats'][1]
+    data_dict['defense'] = data['stats'][2]
+    data_dict['special-attack'] = data['stats'][3]
     data_dict['sprites'] = data['sprites']['front_shiny']
-
     print(data_dict)
 
 func('zapdos')
@@ -37,8 +43,11 @@ def func(poke):
     data_dict['name'] = data['name']
     data_dict['abilities'] = data['abilities'][0]['ability']
     data_dict['base_experience'] = data['base_experience']
+    data_dict['attack'] = data['stats'][0]
+    data_dict['attack'] = data['stats'][1]
+    data_dict['defense'] = data['stats'][2]
+    data_dict['special-attack'] = data['stats'][3]
     data_dict['sprites'] = data['sprites']['front_shiny']
-
     print(data_dict)
 
 func('lugia')
@@ -50,8 +59,11 @@ def func(poke):
     data_dict['name'] = data['name']
     data_dict['abilities'] = data['abilities'][0]['ability']
     data_dict['base_experience'] = data['base_experience']
+    data_dict['attack'] = data['stats'][0]
+    data_dict['attack'] = data['stats'][1]
+    data_dict['defense'] = data['stats'][2]
+    data_dict['special-attack'] = data['stats'][3]
     data_dict['sprites'] = data['sprites']['front_shiny']
-
     print(data_dict)
 
 func('ditto')
@@ -63,8 +75,25 @@ def func(poke):
     data_dict['name'] = data['name']
     data_dict['abilities'] = data['abilities'][0]['ability']
     data_dict['base_experience'] = data['base_experience']
+    data_dict['attack'] = data['stats'][0]
+    data_dict['attack'] = data['stats'][1]
+    data_dict['defense'] = data['stats'][2]
+    data_dict['special-attack'] = data['stats'][3]
     data_dict['sprites'] = data['sprites']['front_shiny']
-
     print(data_dict)
 
 func('mew')
+
+# pokemon properties to include:
+#                 pokemon name
+#        from the stats section:
+#                  base stat for hp
+#                  base stat for defense
+#                  base stat for attack
+#        from the sprites section:
+#                  front_shiny (URL to the image) 
+#                  or any other image you like more 
+#        from the abilities section:
+#                  At Least One Ability
+# and any other properties you might find that 
+# interest you.
